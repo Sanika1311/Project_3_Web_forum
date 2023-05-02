313a52577ff79a974d7406cf2877d13020c4fc2f
Sanika1311 <58742785+Sanika1311@users.noreply.github.com>
2023-05-01 23:42:44 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-set -e # exit immediately if newman complains
-trap 'kill $PID' EXIT # kill the server on exit
+#set -e # exit immediately if newman complains
+#trap 'kill $PID' EXIT # kill the server on exit
 
-./run.sh &
-PID=$! # record the PID
+#./run.sh &
+#PID=$! # record the PID
 
-newman run Basic Test.postman_collection.json -e env.json # use the env file
-newman run Basic Test.postman_collection.json -n 50 # 50 iterations
+#newman run Basic Test.postman_collection.json -e env.json # use the env file
+#newman run Basic Test.postman_collection.json -n 50 # 50 iterations
